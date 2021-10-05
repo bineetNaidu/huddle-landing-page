6fba3e0a1c89db59b97ea2d404197d54ed17766d
bineetNaidu <bineet.s.naidu@gmail.com>
2021-10-05 23:13:53 +0530

    🚀 add theme and Global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
+import { ThemeProvider } from 'styled-components';
 import { Container } from './components/styles/Container.styled';
 import { Header } from './components/Header';
+import { GlobalStyles } from './components/styles/GlobalStyles';
+
+const theme = {
+  colors: {
+    header: '#ebfbff',
+    body: '#fff',
+    footer: '#003333',
+  },
+};
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
       <Header />
       <Container>
         <h1>Hello World</h1>
       </Container>
-    </>
+    </ThemeProvider>
   );
 }
 
--- src/components/styles/Header.styled.tsx
+++ src/components/styles/Header.styled.tsx
@@ -5,6 +5,6 @@ interface HeaderProps {
 }
 
 export const StyledHeader = styled.header<HeaderProps>`
-  background-color: ${(props) => props.bg || '#ebfbff'};
+  background-color: ${(props) => props.bg || props.theme.colors.header};
   padding: 40px 0;
 `;
